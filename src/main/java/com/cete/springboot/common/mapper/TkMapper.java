cb0b92b02e5c944747e330d60957f08f776659f7
wmtxg <wmtxg@126.com>
2019-12-16 19:41:28 +0800

    22

--- src/main/java/com/cete/springboot/common/mapper/TkMapper.java
+++
@@ -1,8 +0,0 @@
-package com.cete.springboot.common.mapper;
-
-import tk.mybatis.mapper.common.Mapper;
-import tk.mybatis.mapper.common.MySqlMapper;
-
-public interface TkMapper<T> extends Mapper<T>, MySqlMapper<T> {
-
-}
